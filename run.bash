6c1eb456f8853445c906116580c09d45a2346db0
Beatriz Luna <luna@br005.pvt.bridges.psc.edu>
2018-11-09 13:39:43 -0500

    it works :)

--- run.bash
+++ run.bash
@@ -8,15 +8,13 @@ for sub_dir in scratch/T1/sub-*/; do
    logfile=$SCRATCH/log/log-$SUBJ.txt
 
    # does the directory exist already?
-   # TODO 1: replace ... with FS directory for $SUBJ
-   [ -d .... ] && echo "file exists" && continue
+   [ -d $SUBJECTS_DIR/$SUBJ ] && echo "file exists" && continue
 
    # check that subject doesn't already exist
    # squeue -A $(id -gn) # find name switch
     squeue -A $(id -gn) | grep $SUBJ && echo "have name in queue"  && continue
 
    # submit job to batch queue
-   # TODO 2: remove echo
-   echo sbatch -o $logfile -e $logfile -J $SUBJ fs_batch_cmd.bash
+    sbatch -o $logfile -e $logfile -J $SUBJ fs_batch_cmd.bash
 done
 
